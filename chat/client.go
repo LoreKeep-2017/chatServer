7bf41cfe7e02b88aa7442375dddd5505f7fbd801
Ilia Bulavintsev <i@izibots.io>
2017-12-04 14:45:56 +0300

    aa

--- chat/client.go
+++ chat/client.go
@@ -50,8 +50,7 @@ func NewClient(ws *websocket.Conn, server *Server, room *Room) *Client {
 	doneCh := make(chan bool)
 	addRoomCh := make(chan *Room)
 	delRoomCh := make(chan *Room)
-	nick := "User_" + strconv.Itoa((*room).Id)
-	return &Client{Id: maxId, ws: ws, server: server, room: room, ch: ch, doneCh: doneCh, addRoomCh: addRoomCh, delRoomCh: delRoomCh, Nick: nick}
+	return &Client{Id: maxId, ws: ws, server: server, room: room, ch: ch, doneCh: doneCh, addRoomCh: addRoomCh, delRoomCh: delRoomCh}
 }
 
 func (c *Client) Conn() *websocket.Conn {
